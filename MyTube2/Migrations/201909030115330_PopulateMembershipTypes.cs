8c2e5aaa2ba0571fd8882ed91e83eba79cb31dc4
deyjoy <joydey@gmail.com>
2019-09-02 21:24:32 -0400

    Initial commit with basic structures

---
+++ MyTube2/Migrations/201909030115330_PopulateMembershipTypes.cs
@@ -0,0 +1,16 @@
+namespace MyTube2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ MyTube2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTube2.Startup))]
+namespace MyTube2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
